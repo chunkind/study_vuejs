9ff7595e3ebd1abe270bddfbeb4671ae6e8d7997
chunkind <chunkind88@gmail.com>
2020-01-29 13:50:13 +0900

    no message

---
+++ 11_vuex/countryapp/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <region-buttons></region-buttons>
+    <country-list></country-list>
+  </div>
+</template>
+
+<script>
+import RegionButtons from './components/RegionButton';
+import CountryList from './components/CountryList';
+
+export default {
+  name : 'app',
+  components:{RegionButtons, CountryList}
+}
+</script>
+<style>
+
+</style>
---
+++ 11_vuex/countryapp/src/components/RegionButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <button class="region" v-for="region in regions" :key="region"
+            v-bind:class="isSelected(region)"
+            @click="changeRegion({region:region})">
+            {{region}}
+        </button>
+    </div>
+</template>
+
+<script>
+import Constant from '../Constant'
+import { mapGetters, mapMutations } from 'vuex'
+export default {
+    name : "RegionButtons",
+    // computed : {
+    //     regions() {
+    //         return this.$store.getters.regions;
+    //     },
+    //     currentRegion() {
+    //         return this.$store.getters.currentRegion;
+    //     }
+    // },
+    //간결화 핼퍼메서드 사용
+    computed : mapGetters(['regions', 'currentRegion']),
+    methods : {
+        isSelected(region){
+            if(region == this.currentRegion) return {selected:true};
+            else return {selected:false};
+        },
+        ...mapMutations([
+            Constant.CHANGE_REGION
+        ])
+    }
+}
+</script>
+
+<style>
+    button.region { text-align: center; width:80px; margin:2px; border:solid 1px gray;  }
+    button.selected { background-color:purple; color:aqua; }
+</style>
